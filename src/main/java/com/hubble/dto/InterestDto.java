c8626f9651b808ab90c0a75834c899f0c19a9eec
prox-b <proxhubble@gmail.com>
2025-10-22 16:56:34 +0900

    Create InterestDto.java

---
+++ src/main/java/com/hubble/dto/InterestDto.java
@@ -0,0 +1,18 @@
+package com.hubble.dto;
+
+public class InterestDto {
+    
+    private Long id;
+    private String name;
+    private String slug;
+    private String icon;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getSlug() { return slug; }
+    public void setSlug(String slug) { this.slug = slug; }
+    public String getIcon() { return icon; }
+    public void setIcon(String icon) { this.icon = icon; }
+}
