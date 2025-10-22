4569e0f3dd34a567cd347efeae2009238b64038a
prox-b <proxhubble@gmail.com>
2025-10-22 17:35:16 +0900

    Create UpdateCommentRequest.java

---
+++ src/main/java/com/hubble/dto/UpdateCommentRequest.java
@@ -0,0 +1,14 @@
+package com.hubble.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UpdateCommentRequest {
+    
+    @NotBlank(message = "Content is required")
+    @Size(max = 5000, message = "Content must be less than 5000 characters")
+    private String content;
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
