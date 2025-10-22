a406f88038993e9c2a03a0c06414c6be02e82005
prox-b <proxhubble@gmail.com>
2025-10-22 17:34:38 +0900

    Create SeriesSimpleDto.java

---
+++ src/main/java/com/hubble/dto/SeriesSimpleDto.java
@@ -0,0 +1,14 @@
+package com.hubble.dto;
+
+public class SeriesSimpleDto {
+    private Long id;
+    private String title;
+    private String slug;
+    
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getSlug() { return slug; }
+    public void setSlug(String slug) { this.slug = slug; }
+}
